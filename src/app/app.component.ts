dd91c889938c6635c8e4687925506e8847b61ad9
Kofi Morrison <kofiamu.morrison@emea.nttdata.com>
2024-03-20 00:27:37 +0100

    first submission

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,327 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomePageComponent } from "./home-page/home-page.component";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import Swiper from 'swiper';
+import Isotope from 'isotope-layout';
+import GLightbox from 'glightbox';
+import PureCounter from "@srexi/purecounterjs";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, HomePageComponent]
 })
 export class AppComponent {
   title = 'rosen-stolz-app';
+
+  scrollRef = 0;
+
+  constructor() {
+    window.addEventListener('wheel', (event) => {
+      console.log(999)
+    });
+  }
+
+    ngOnInit(): void {
+      // window.addEventListener('load', () => {
+      //   AOS.init({
+      //     duration: 1000,
+      //     easing: "ease-in-out",
+      //     once: true,
+      //     mirror: false
+      //   });
+      // });
+
+      (function() {
+        /**
+         * Easy selector helper function
+         */
+        const select = (el: string, all = false): any => {
+          el = el.trim()
+          if (all) {
+            return [...document.querySelectorAll(el) as any]
+          } else {
+            return document.querySelector(el)
+          }
+        }
+
+        /**
+         * Easy event listener function
+         */
+        const on = (type: string, el: string, listener: { (e: any): void; (e: any): void; (e: any): void; }, all = false) => {
+          if (all) {
+            select(el, all).forEach((e: { addEventListener: (arg0: string, arg1: { (e: any): void; (e: any): void; (e: any): void; }) => any; }) => e.addEventListener(type, listener))
+          } else {
+            select(el, all).addEventListener(type, listener)
+          }
+        }
+
+        /**
+         * Easy on scroll event listener
+         */
+        const onscroll = (el: Document, listener: { (): void; (): void; (): void; }) => {
+          el.addEventListener('scroll', listener)
+        }
+
+        /**
+         * Navbar links active state on scroll
+         */
+        let navbarlinks = select('#navbar .scrollto', true)
+        const navbarlinksActive = () => {
+          let position = window.scrollY + 200
+          navbarlinks.forEach((navbarlink: { hash: string; classList: { add: (arg0: string) => void; remove: (arg0: string) => void; }; }) => {
+            if (!navbarlink.hash) return
+            let section = select(navbarlink.hash)
+            if (!section) return
+            if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+              navbarlink.classList.add('active')
+            } else {
+              navbarlink.classList.remove('active')
+            }
+          })
+        }
+        window.addEventListener('load', navbarlinksActive)
+        onscroll(document, navbarlinksActive)
+
+        /**
+         * Scrolls to an element with header offset
+         */
+        const scrollto = (el: string) => {
+          let header = select('#header')
+          let offset = header.offsetHeight
+
+          if (!header.classList.contains('header-scrolled')) {
+            offset -= 10
+          }
+
+          let elementPos = select(el).offsetTop
+          window.scrollTo({
+            top: elementPos - offset,
+            behavior: 'smooth'
+          })
+        }
+
+        /**
+         * Toggle .header-scrolled class to #header when page is scrolled
+         */
+        let selectHeader = select('#header')
+        if (selectHeader) {
+          const headerScrolled = () => {
+            if (window.scrollY > 100) {
+              selectHeader.classList.add('header-scrolled')
+            } else {
+              selectHeader.classList.remove('header-scrolled')
+            }
+          }
+          window.addEventListener('load', headerScrolled)
+          onscroll(document, headerScrolled)
+        }
+
+        /**
+         * Back to top button
+         */
+        let backtotop = select('.back-to-top')
+        if (backtotop) {
+          const toggleBacktotop = () => {
+            if (window.scrollY > 100) {
+              backtotop.classList.add('active')
+            } else {
+              backtotop.classList.remove('active')
+            }
+          }
+          window.addEventListener('load', toggleBacktotop)
+          onscroll(document, toggleBacktotop)
+        }
+
+        /**
+         * Mobile nav toggle
+         */
+        on('click', '.mobile-nav-toggle', function(e) {
+          select('#navbar').classList.toggle('navbar-mobile')
+          e.classList.toggle('bi-list')
+          e.classList.toggle('bi-x')
+        })
+
+        /**
+         * Mobile nav dropdowns activate
+         */
+        on('click', '.navbar .dropdown > a', function(e) {
+          if (select('#navbar').classList.contains('navbar-mobile')) {
+            e.preventDefault()
+            e.nextElementSibling.classList.toggle('dropdown-active')
+          }
+        }, true)
+
+        /**
+         * Scrool with ofset on links with a class name .scrollto
+         */
+        on('click', '.scrollto', function(e) {
+          if (select(e.hash)) {
+            e.preventDefault()
+
+            let navbar = select('#navbar')
+            if (navbar.classList.contains('navbar-mobile')) {
+              navbar.classList.remove('navbar-mobile')
+              let navbarToggle = select('.mobile-nav-toggle')
+              navbarToggle.classList.toggle('bi-list')
+              navbarToggle.classList.toggle('bi-x')
+            }
+            scrollto(e.hash)
+          }
+        }, true)
+
+        /**
+         * Scroll with ofset on page load with hash links in the url
+         */
+        window.addEventListener('load', () => {
+          if (window.location.hash) {
+            if (select(window.location.hash)) {
+              scrollto(window.location.hash)
+            }
+          }
+        });
+
+        /**
+         * Clients Slider
+         */
+        new Swiper('.clients-slider', {
+          speed: 400,
+          loop: true,
+          autoplay: {
+            delay: 5000,
+            disableOnInteraction: false
+          },
+          slidesPerView: 'auto',
+          pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: true
+          },
+          breakpoints: {
+            320: {
+              slidesPerView: 2,
+              spaceBetween: 40
+            },
+            480: {
+              slidesPerView: 3,
+              spaceBetween: 60
+            },
+            640: {
+              slidesPerView: 4,
+              spaceBetween: 80
+            },
+            992: {
+              slidesPerView: 6,
+              spaceBetween: 120
+            }
+          }
+        });
+
+        /**
+         * Porfolio isotope and filter
+         */
+        window.addEventListener('load', () => {
+          let portfolioContainer = select('.portfolio-container');
+          if (portfolioContainer) {
+            let portfolioIsotope = new Isotope(portfolioContainer, {
+              itemSelector: '.portfolio-item',
+              layoutMode: 'fitRows'
+            });
+
+            let portfolioFilters = select('#portfolio-flters li', true);
+
+            on('click', '#portfolio-flters li', function(e) {
+              e.preventDefault();
+              portfolioFilters.forEach(function(el: { classList: { remove: (arg0: string) => void; }; }) {
+                el.classList.remove('filter-active');
+              });
+              e.classList.add('filter-active');
+
+              portfolioIsotope.arrange({
+                filter: e.getAttribute('data-filter')
+              });
+              aos_init();
+            }, true);
+          }
+
+        });
+
+        /**
+         * Initiate portfolio lightbox
+         */
+        const portfolioLightbox = GLightbox({
+          selector: '.portfokio-lightbox'
+        });
+
+        /**
+         * Portfolio details slider
+         */
+        new Swiper('.portfolio-details-slider', {
+          speed: 400,
+          autoplay: {
+            delay: 5000,
+            disableOnInteraction: false
+          },
+          pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: true
+          }
+        });
+
+        /**
+         * Testimonials slider
+         */
+        new Swiper('.testimonials-slider', {
+          speed: 600,
+          loop: true,
+          autoplay: {
+            delay: 5000,
+            disableOnInteraction: false
+          },
+          slidesPerView: 'auto',
+          pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: true
+          },
+          breakpoints: {
+            320: {
+              slidesPerView: 1,
+              spaceBetween: 40
+            },
+
+            1200: {
+              slidesPerView: 3,
+            }
+          }
+        });
+
+        /**
+         * Animation on scroll
+         */
+        function aos_init() {
+          AOS.init({
+            duration: 700,
+            easing: "ease-in-out",
+            once: false,
+            mirror: false
+          });
+        }
+        window.addEventListener('load', () => {
+          aos_init();
+        });
+
+        /**
+         * Initiate Pure Counter
+         */
+        new PureCounter();
+
+      })();
+
+  }
 }
---
+++ src/app/home-page/home-page.component.ts
@@ -0,0 +1,11 @@
+import { Component, AfterViewInit, OnInit, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-home-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-page.component.html',
+  styleUrl: './home-page.component.css'
+})
+export class HomePageComponent{
+}
