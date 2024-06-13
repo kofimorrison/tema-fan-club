4f33a13c78a50b1889625f17483896f8928a00a9
Kofi Morrison <kofiamu.morrison@emea.nttdata.com>
2024-06-13 02:45:13 +0200

    fix email

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -77,6 +77,8 @@ export class HomePageComponent implements OnInit, AfterViewInit {
   };
 
   ngAfterViewInit() {
+
+    this.initializeFormHandling();
     AOS.init({
       duration: 700,
       easing: 'ease-in-out',
@@ -201,6 +203,87 @@ export class HomePageComponent implements OnInit, AfterViewInit {
       mirror: false,
     });
   }
+
+  initializeFormHandling() {
+    "use strict";
+
+    let forms = document.querySelectorAll('.php-email-form');
+
+    forms.forEach((form) => {
+      form.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        let thisForm = form as HTMLFormElement;
+
+        let action = thisForm.getAttribute('action');
+        let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
+
+        if (!action) {
+          this.displayError(thisForm, 'The form action property is not set!');
+          return;
+        }
+        thisForm.querySelector('.loading')?.classList.add('d-block');
+        thisForm.querySelector('.error-message')?.classList.remove('d-block');
+        thisForm.querySelector('.sent-message')?.classList.remove('d-block');
+
+        let formData = new FormData(thisForm);
+
+        if (recaptcha) {
+          if (typeof recaptcha !== "undefined") {
+            (recaptcha as any).ready(() => {
+              try {
+                (recaptcha as any).execute(recaptcha, { action: 'php_email_form_submit' })
+                  .then((token: string | Blob) => {
+                    formData.set('recaptcha-response', token);
+                    this.phpEmailFormSubmit(thisForm, action, formData);
+                  });
+              } catch (error) {
+                this.displayError(thisForm, error);
+              }
+            });
+          } else {
+            this.displayError(thisForm, 'The reCaptcha javascript API url is not loaded!');
+          }
+        } else {
+          this.phpEmailFormSubmit(thisForm, action, formData);
+        }
+      });
+    });
+  }
+
+  phpEmailFormSubmit(thisForm: HTMLFormElement, action: string, formData: FormData) {
+    fetch(action, {
+      method: 'POST',
+      body: formData,
+      headers: { 'X-Requested-With': 'XMLHttpRequest' }
+    })
+      .then(response => {
+        if (response.ok) {
+          return response.text();
+        } else {
+          throw new Error(`${response.status} ${response.statusText} ${response.url}`);
+        }
+      })
+      .then(data => {
+        thisForm.querySelector('.loading')?.classList.remove('d-block');
+        if (data.trim() === 'OK') {
+          thisForm.querySelector('.sent-message')?.classList.add('d-block');
+          thisForm.reset();
+        } else {
+          throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action);
+        }
+      })
+      .catch((error) => {
+        this.displayError(thisForm, error);
+      });
+  }
+
+  displayError(thisForm: HTMLFormElement, error: any) {
+    thisForm.querySelector('.loading')?.classList.remove('d-block');
+    thisForm.querySelector('.error-message')!.innerHTML = error;
+    thisForm.querySelector('.error-message')?.classList.add('d-block');
+  }
+
 
   ngOnInit(): void {
     if (window.history.state?.page) {
